2226907542a38871787c68daf22336db5eed6359
Viresh Kumar <viresh.kumar@linaro.org>
2014-03-20 13:44:21 +0530

    timer: track pinned timers with TIMER_PINNED flag
    In order to quiesce a CPU on which Isolation might be required, we need to move
    away all the timers queued on that CPU. There are two types of timers queued on
    any CPU: ones that are pinned to that CPU and others can run on any CPU but are
    queued on CPU in question. And we need to migrate only the second type of timers
    away from the CPU entering quiesce state.
    
    For this we need some basic infrastructure in timer core to identify which
    timers are pinned and which are not.
    
    Hence, this patch adds another flag bit TIMER_PINNED which will be set only for
    the timers which are pinned to a CPU.
    
    It also removes 'pinned' parameter of __mod_timer() as it is no more required.
    
    NOTE: One functional change worth mentioning
    
    Existing Behavior: add_timer_on() followed by multiple mod_timer() wouldn't pin
    the timer on CPU mentioned in add_timer_on()..
    
    New Behavior: add_timer_on() followed by multiple mod_timer() would pin the
    timer on CPU running mod_timer().
    
    I didn't gave much attention to this as we should call mod_timer_on() for the
    timers queued with add_timer_on(). Though if required we can simply clear the
    TIMER_PINNED flag in mod_timer().
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- kernel/timer.c
+++ kernel/timer.c
@@ -103,6 +103,11 @@ static inline unsigned int tbase_get_irqsafe(struct tvec_base *base)
 	return ((unsigned int)(unsigned long)base & TIMER_IRQSAFE);
 }
 
+static inline unsigned int tbase_get_pinned(struct tvec_base *base)
+{
+	return ((unsigned int)(unsigned long)base & TIMER_PINNED);
+}
+
 static inline struct tvec_base *tbase_get_base(struct tvec_base *base)
 {
 	return ((struct tvec_base *)((unsigned long)base & ~TIMER_FLAG_MASK));
@@ -114,6 +119,13 @@ timer_set_base(struct timer_list *timer, struct tvec_base *new_base)
 	unsigned long flags = (unsigned long)timer->base & TIMER_FLAG_MASK;
 
 	timer->base = (struct tvec_base *)((unsigned long)(new_base) | flags);
+}
+
+static inline void
+timer_set_flags(struct timer_list *timer, unsigned int flags)
+{
+	timer->base = (struct tvec_base *)((unsigned long)(timer->base) |
+					   flags);
 }
 
 static unsigned long round_jiffies_common(unsigned long j, int cpu,
@@ -721,8 +733,7 @@ static struct tvec_base *lock_timer_base(struct timer_list *timer,
 }
 
 static inline int
-__mod_timer(struct timer_list *timer, unsigned long expires,
-						bool pending_only, int pinned)
+__mod_timer(struct timer_list *timer, unsigned long expires, bool pending_only)
 {
 	struct tvec_base *base, *new_base;
 	unsigned long flags;
@@ -739,7 +750,7 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 
 	debug_activate(timer, expires);
 
-	cpu = get_nohz_timer_target(pinned);
+	cpu = get_nohz_timer_target(tbase_get_pinned(base));
 	new_base = per_cpu(tvec_bases, cpu);
 
 	if (base != new_base) {
@@ -781,7 +792,7 @@ out_unlock:
  */
 int mod_timer_pending(struct timer_list *timer, unsigned long expires)
 {
-	return __mod_timer(timer, expires, true, TIMER_NOT_PINNED);
+	return __mod_timer(timer, expires, true);
 }
 EXPORT_SYMBOL(mod_timer_pending);
 
@@ -856,7 +867,7 @@ int mod_timer(struct timer_list *timer, unsigned long expires)
 	if (timer_pending(timer) && timer->expires == expires)
 		return 1;
 
-	return __mod_timer(timer, expires, false, TIMER_NOT_PINNED);
+	return __mod_timer(timer, expires, false);
 }
 EXPORT_SYMBOL(mod_timer);
 
@@ -884,7 +895,8 @@ int mod_timer_pinned(struct timer_list *timer, unsigned long expires)
 	if (timer->expires == expires && timer_pending(timer))
 		return 1;
 
-	return __mod_timer(timer, expires, false, TIMER_PINNED);
+	timer_set_flags(timer, TIMER_PINNED);
+	return __mod_timer(timer, expires, false);
 }
 EXPORT_SYMBOL(mod_timer_pinned);
 
@@ -923,6 +935,7 @@ void add_timer_on(struct timer_list *timer, int cpu)
 
 	timer_stats_timer_set_start_info(timer);
 	BUG_ON(timer_pending(timer) || !timer->function);
+	timer_set_flags(timer, TIMER_PINNED);
 	spin_lock_irqsave(&base->lock, flags);
 	timer_set_base(timer, base);
 	debug_activate(timer, timer->expires);
@@ -1461,7 +1474,7 @@ signed long __sched schedule_timeout(signed long timeout)
 	expire = timeout + jiffies;
 
 	setup_timer_on_stack(&timer, process_timeout, (unsigned long)current);
-	__mod_timer(&timer, expire, false, TIMER_NOT_PINNED);
+	__mod_timer(&timer, expire, false);
 	schedule();
 	del_singleshot_timer_sync(&timer);
 
